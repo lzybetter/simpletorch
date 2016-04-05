e804d9b45a61fc8b72742069795b333814b453b9
LiZhenYang <li.zhenyang@ascl.jlu.edu.cn>
2016-04-05 10:59:37 +0800

    完成基本功能。

--- src/com/lzybetter/simpletorch/MainActivity.java
+++ src/com/lzybetter/simpletorch/MainActivity.java
@@ -1,16 +1,73 @@
 package com.lzybetter.simpletorch;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
+	
+	private ToggleButton switchButton;
+	private Camera camera;
+	private RelativeLayout background;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		
+		background = (RelativeLayout)findViewById(R.id.background);
+		switchButton = (ToggleButton)findViewById(R.id.switch_button);
+		SwitchButtonListener switchButtonListener = new SwitchButtonListener();
+		switchButton.setOnClickListener(switchButtonListener);
+		
+	}
+	
+	class SwitchButtonListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			ToggleButton button = (ToggleButton)v;
+			if(!button.isChecked()){
+				if(camera == null){
+					camera = Camera.open();
+				}
+				Parameters p = camera.getParameters();
+				p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+				camera.setParameters(p);
+				camera.startPreview();
+				button.setButtonDrawable(R.drawable.open);
+				background.setBackgroundColor(Color.BLACK);
+			}else {
+				if(camera == null){
+					camera = Camera.open();
+				}
+				Parameters p = camera.getParameters();
+				p.setFlashMode(Parameters.FLASH_MODE_OFF);
+				camera.setParameters(p);
+				camera.stopPreview();
+				background.setBackgroundColor(Color.WHITE);
+				button.setButtonDrawable(R.drawable.close);
+				if(camera != null){
+					camera.setPreviewCallback(null);
+					camera.stopPreview();
+					camera.release();
+					camera = null; 
+				}
+			}
+		}
+		
 	}
 
 	@Override
